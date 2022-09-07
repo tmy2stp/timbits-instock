8da13fb52ad9b10c44a12bee5d8369ac5688a904
Ani Mihaylova <anivm.webdev@gmail.com>
2022-09-07 15:17:41 -0400

    adds some styling to the inventory form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import InventoryForm from './components/InventoryForm/InventoryForm';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return(
-    <BrowserRouter>
-      <Routes>
+    <>
       <InventoryForm formType={"Add New"}/>
-        {/* <Route path="/" element={}/>
-        <Route path="/warehouses" element={}/>
-        <Route path="/warehouses/:id" element={}/>
-        <Route path="/warehouses/:id/edit" element={}/>
-        <Route path="/warehouses/add-new" element={}/>
-        <Route path="/inventory" element={}/>
-        <Route path="/inventory/:id" element={}/>
-        <Route path="/inventory/:id/edit" element={}/>
-        <Route path="/inventory/add-new" element={}/> */}
-      </Routes>
-    </BrowserRouter>
+    </>
+
+    // // <BrowserRouter>
+    // //   <Routes>
+    //     {/* <Route path="/" element={}/>
+    //     <Route path="/warehouses" element={}/>
+    //     <Route path="/warehouses/:id" element={}/>
+    //     <Route path="/warehouses/:id/edit" element={}/>
+    //     <Route path="/warehouses/add-new" element={}/>
+    //     <Route path="/inventory" element={}/>
+    //     <Route path="/inventory/:id" element={}/>
+    //     <Route path="/inventory/:id/edit" element={}/>
+    //     <Route path="/inventory/add-new" element={}/> */}
+    // //   </Routes>
+    // // </BrowserRouter>
   );
 }
 
