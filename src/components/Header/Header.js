780025a04819621edbb1d55c265b3782aac24a52
Thomas Morgan <tommorgan86@gmail.com>
2022-09-07 18:35:31 -0400

    established header component and built it out. Need to add styling and logo, skeleton structure for now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,33 @@
 import './Header.scss'
+// import logo from "./"
+// import { Link, NavLink } from "react-router-dom"
 
 const Header = () => {
 
+    return (
+        <header className="header">
+            <nav className="header__nav">
+                <Link to="/" className="headerLOGO">
+                    <img src={LOGOHERE} alt="instock logo" />
+                </Link>
+
+            <div className="NAV BUTTONZ">
+                <NavLink to="/warehouses" className="">
+                    WAREHOUSES
+                </NavLink>
+
+                <NavLink to="/inventory" className="">
+                    INVENTORY
+                </NavLink>
+            </div>
+            </nav>
+        </header>
+    )
+    // MAKE CLASS FOR ACTIVE LINK!!!!
+    // navbar 
+    // logo link
+    // warehouses aspect left (buttons)
+    // inventory aspect right (buttons)
 
 }
 
