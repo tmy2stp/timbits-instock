a3be0644e90f803798f098c6569c15e1223d39b1
simon tang <simont.codes@gmail.com>
2022-09-09 17:10:13 -0400

    checking out nastassia branch

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,6 +1,8 @@
 import "./WarehouseList.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import deleteIcon from '../../assets/images/delete_outline-24px.svg'
+import editIcon from  '../../assets/images/edit-24px.svg'
 
 function WarehouseList() {
   const [allWarehouses, setAllWarehouses] = useState([]);
@@ -18,16 +20,37 @@ function WarehouseList() {
       <header className="warehouse__header">
         <h1>Warehouses</h1>
         <form action="">
-          <input type="text" placeholder="Search...." className="warehouse__header-input" />
+          <input
+            type="text"
+            placeholder="Search...."
+            className="warehouse__header-input"
+          />
         </form>
 
         <button className="warehouse__header-button inventory-form__button--before">
           + Add New Warehouse
         </button>
       </header>
-
       {allWarehouses.map((warehouse) => {
-        return <h2>{warehouse.name}</h2>;
+        return (
+          <div className="warehouse__card">
+            <h5>WAREHOUSE</h5>
+            <h5>{warehouse.name}</h5>
+            <h5>ADDRESS</h5>
+            <h5>{warehouse.address}</h5>
+            <h5>CONTACT NAME</h5>
+            <h5>{warehouse.contact.name}</h5>
+            <h5>CONTACT INFORMATION</h5>
+            <div>
+              <h5>{warehouse.contact.phone}</h5>
+              <h5>{warehouse.contact.email}</h5>
+            </div>
+            <div>
+              <img src={deleteIcon} alt="delete button" />
+              <img src={editIcon} alt="edit button" />
+            </div>
+          </div>
+        );
       })}
     </div>
   );
