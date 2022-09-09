75198cb0d76e762796dfe86fc9a9ba87631fe303
Анастасия Макаренко <anastasiia@Anastasias-Air.localdomain>
2022-09-09 13:08:50 -0400

    create axios request and get data from api

--- src/components/WarehouseInventoryList/WarehouseInventoryList.js
+++ src/components/WarehouseInventoryList/WarehouseInventoryList.js
@@ -1,8 +1,29 @@
 import "./WarehouseInventoryList.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 function WarehouseInventoryList() {
+  //State for selected warehouse's inventory data
+  const [warehouseInventory, setWarehouseInventory] = useState();
+
+  const {id} = useParams();
+
+  //Retrieve data from API
+  useEffect (()=>{
+    axios
+        .get(`http://localhost:8080/inventories/warehouse/${id}`)
+        .then((response)=>{
+            const inventories = response.data;
+            console.log(inventories);
+            setWarehouseInventory(inventories);
+        })
+}, [])
+
   return (
-    <div>WarehouseInventoryList</div>
+    <div>
+      
+    </div>
   )
 }
 
