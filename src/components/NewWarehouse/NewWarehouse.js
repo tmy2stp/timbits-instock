aa24fe88064a63ac26f5f788e708d1d2d713a607
katia santos <katiamsa@gmail.com>
2022-09-09 16:08:01 -0400

    added axios post request and state

--- src/components/NewWarehouse/NewWarehouse.js
+++ src/components/NewWarehouse/NewWarehouse.js
@@ -1,93 +1,221 @@
 import "./NewWarehouse.scss";
 import backArrow from "../../assets/images/arrow_back-24px.svg";
+import axios from "axios";
+import { useState } from "react";
+const API_URL = "http://localhost:8080";
 
 function NewWarehouse() {
+  // const [visibleItem, setVisibleItem] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [managerName, setManagerName] = useState("");
+  const [managerPosition, setManagerPosition] = useState("");
+  const [managerPhone, setManagerPhone] = useState("");
+  const [managerEmail, setManagerEmail] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChangeAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handleChangeCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const handleChangeContactName = (event) => {
+    setManagerName(event.target.value);
+  };
+
+  const handleChangePosition = (event) => {
+    setManagerPosition(event.target.value);
+  };
+
+  const handleChangePhone = (event) => {
+    setManagerPhone(event.target.value);
+  };
+
+  const handleChangeEmail = (event) => {
+    setManagerEmail(event.target.value);
+  };
+
+  // created form validation
+  const isFormValid = () => {
+    if (
+      !name ||
+      !address ||
+      !city ||
+      !managerPhone ||
+      !country ||
+      !managerName ||
+      !managerPosition ||
+      !managerEmail
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  const newWarehouse = {
+    name: name,
+    address: address,
+    city: city,
+    country: country,
+    contact: {
+      managerName: managerName,
+      managerPosition: managerPosition,
+      managerPhone: managerPhone,
+      managerEmail: managerEmail,
+    },
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isFormValid()) {
+      axios
+        .post(API_URL + "/warehouses", newWarehouse)
+        .then((response) => {
+          console.log(response.data);
+          // setVisibleItem("message");
+          setName("");
+          setAddress("");
+          setCity("");
+          setCountry("");
+          setManagerPhone("");
+          setManagerName("");
+          setManagerPosition("");
+          setManagerEmail("");
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+        });
+    } else {
+      setIsError(true);
+    }
+  };
+  console.log(
+    managerEmail,
+    managerPosition,
+    managerName,
+    country,
+    managerPhone,
+    city,
+    address,
+    name
+  );
+
   return (
-    <div className="inventory-form">
-      <header className="inventory-form__header">
+    <div className="newWarehouse-form">
+      <header className="newWarehouse-form__header">
         <img
-          className="inventory-form__back"
+          className="newWarehouse-form__back"
           src={backArrow}
           alt="arrow to navigate back"
         />
-        <h1 className="inventory-form__title"> Add New Warehouse</h1>
+        <h1 className="newWarehouse-form__title"> Add New Warehouse</h1>
       </header>
-      <form className="inventory-form__form">
-        <div className="inventory-form__section inventory-form__section--left">
-          <h2 className="inventory-form__subtitle">Warehouse Details</h2>
-          <label className="inventory-form__label">
+      <form onSubmit={handleSubmit} className="newWarehouse-form__form">
+        <div className="newWarehouse-form__section newWarehouse-form__section--left">
+          <h2 className="newWarehouse-form__subtitle">Warehouse Details</h2>
+          <label className="newWarehouse-form__label">
             Warehouse Name
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Warehouse Name"
+              onChange={handleChangeName}
+              value={name}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             Street Address
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Street Address"
+              onChange={handleChangeAddress}
+              value={address}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             City
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="City"
+              onChange={handleChangeCity}
+              value={city}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             Country
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Country"
+              onChange={handleChangeCountry}
+              value={country}
             />
           </label>
         </div>
-        <div className="inventory-form__section">
-          <h2 className="inventory-form__subtitle">Contact Details</h2>
-          <label className="inventory-form__label">
+        <div className="newWarehouse-form__section">
+          <h2 className="newWarehouse-form__subtitle">Contact Details</h2>
+          <label className="newWarehouse-form__label">
             Contact Name
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Contact Name"
+              onChange={handleChangeContactName}
+              value={managerName}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             Position
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Position"
+              onChange={handleChangePosition}
+              value={managerPosition}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             Phone Number
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Phone Number"
+              onChange={handleChangePhone}
+              value={managerPhone}
             />
           </label>
-          <label className="inventory-form__label">
+          <label className="newWarehouse-form__label">
             Email
             <input
-              className="inventory-form__input"
+              className="newWarehouse-form__input"
               type="text"
               placeholder="Email"
+              onChange={handleChangeEmail}
+              value={managerEmail}
             />
           </label>
         </div>
-        <footer className="inventory-form__footer">
-          <button className="inventory-form__button inventory-form__button--cancel">
+        <footer className="newWarehouse-form__footer">
+          <button className="newWarehouse-form__button newWarehouse-form__button--cancel">
             Cancel
           </button>
-          <button className="inventory-form__button inventory-form__button--before">
+          <button className="newWarehouse-form__button newWarehouse-form__button--before">
             + Add Warehouse
           </button>
         </footer>
