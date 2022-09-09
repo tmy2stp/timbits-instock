5f7d57c1feb0982a50b8ff6ad5b1ed70a3a72318
Анастасия Макаренко <anastasiia@Anastasias-MacBook-Air.local>
2022-09-08 21:57:31 -0400

    fix styling, add mobile styling for warehouse details

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -37,27 +37,29 @@ function WarehouseDetails() {
 
     return (
         <>
-            <div className="warehouse-details">
+            <div className="warehouse-details__top">
                 <div className="warehouse-details__left">
                     <img className="warehouse-details__icon" src={BackArrowIcon} alt="Back arrow"/>
                     <h1 className="warehouse-details__title">{warehouseDetails.city}</h1>
                 </div>
                 <button className="warehouse-details__edit-button">{width<767 ? null : "Edit"}</button>
             </div>
-            <div className="inventory-list">
-                <div className="inventory-list__address-container">
-                    <p className="">Warehouse address:</p>
-                    <p>{warehouseDetails.address}, {warehouseDetails.city}, {warehouseDetails.country}</p>
+            <div className="warehouse-details__bottom">
+                <div className="warehouse-details__address">
+                    <h4 className="warehouse-details__label">Warehouse address:</h4>
+                    <p className="warehouse-details__content">{warehouseDetails.address}, {warehouseDetails.city}, {warehouseDetails.country}</p>
                 </div>
-                <div>
-                    <p>Contact name:</p>
-                    <p>{warehouseDetails.contact.name}</p>
-                    <p>{warehouseDetails.contact.position}</p>
-                </div>
-                <div>
-                    <p>Contact information:</p>
-                    <p>{warehouseDetails.contact.phone}</p>
-                    <p>{warehouseDetails.contact.email}</p>
+                <div className="warehouse-details__contacts">
+                    <div className="warehouse-details__contacts-left">
+                        <h4 className="warehouse-details__label">Contact name:</h4>
+                        <p className="warehouse-details__content">{warehouseDetails.contact.name}</p>
+                        <p className="warehouse-details__content">{warehouseDetails.contact.position}</p>
+                    </div>
+                    <div className="warehouse-details__contacts-right">
+                        <h4 className="warehouse-details__label">Contact information:</h4>
+                        <p className="warehouse-details__content">{warehouseDetails.contact.phone}</p>
+                        <p className="warehouse-details__content">{warehouseDetails.contact.email}</p>
+                    </div>
                 </div>
             </div>
         </>
