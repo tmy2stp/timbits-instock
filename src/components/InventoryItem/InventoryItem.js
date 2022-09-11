93051074130c8f1a68964c4a4ab27be44b291185
Анастасия Макаренко <anastasiia@Anastasias-MacBook-Air.local>
2022-09-10 23:51:43 -0400

    fix classes for warehouse inventory list, finish styling and functionality

--- src/components/InventoryItem/InventoryItem.js
+++ src/components/InventoryItem/InventoryItem.js
@@ -15,32 +15,32 @@ function InventoryItem({inventory, i, id}) {
     }
 
     return (
-        <div className={`card ${i===0 ? "card__first" : null}`}>
+        <div className={`item ${i===0 ? "item" : null}`}>
             {/* {clickDelete === true ? <DeleteComponent />: null} */}
-            <div className="card__key-pair card__item">
-                <h4 className="card__label">Inventory item</h4>
-                    <Link to={`/inventory/${id}`} className="card__item-name card__title-container">
-                        <p className="card__content card__title ">{inventory.itemName}</p>
-                        <img src={ArrowRight} alt="Right arrow" className="card__title-icon"/>
+            <div className="item__key-pair item__item">
+                <h4 className="item__label">Inventory item</h4>
+                    <Link to={`/inventory/${id}`} className="item__item-name item__title-container">
+                        <p className="item__content item__title ">{inventory.itemName}</p>
+                        <img src={ArrowRight} alt="Right arrow" className="item__title-icon"/>
                     </Link>
             </div>
-            <div className="card__key-pair card__right card__status">
-                <h4 className="card__label card__status-label">Status</h4>
-                <p className={`card__content ${inventory.status === "In Stock" ? "card__in-stock" : "card__out-of-stock"}`}>
+            <div className="item__key-pair item__right item__status">
+                <h4 className="item__label item__status-label">Status</h4>
+                <p className={`item__content ${inventory.status === "In Stock" ? "item__in-stock" : "item__out-of-stock"}`}>
                     {inventory.status}
                 </p>
             </div>
-            <div className="card__key-pair card__category">
-                <h4 className="card__label">Category</h4>
-                <p className="card__content">{inventory.category}</p>
+            <div className="item__key-pair item__category">
+                <h4 className="item__label">Category</h4>
+                <p className="item__content">{inventory.category}</p>
             </div>
-            <div className="card__key-pair card__right card__qty">
-                <h4 className="card__label">Qty</h4>
-                <p className="card__content">{inventory.quantity}</p>
+            <div className="item__key-pair item__right item__qty">
+                <h4 className="item__label">Qty</h4>
+                <p className="item__content">{inventory.quantity}</p>
             </div>
-            <div className="card__bottom">
-                <div onClick={handleClick} className="card__delete-container"><img className="card__icon" src={DeleteIcon} alt="Delete icon" /></div>
-                <Link to={`/warehouses/edit/${id}`}><img className="card__icon" src={EditIcon} alt="Edit icon"/></Link>
+            <div className="item__bottom">
+                <div onClick={handleClick} className="item__delete-container"><img className="item__icon" src={DeleteIcon} alt="Delete icon" /></div>
+                <Link to={`/warehouses/edit/${id}`}><img className="item__icon" src={EditIcon} alt="Edit icon"/></Link>
             </div>
         </div>
     )
--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import InventoryCard from "../InventoryCard /InventoryCard";
 import "./InventoryList.scss";
+import SortIcon from "../../assets/images/sort-24px.svg";
 
 function InventoryList() {
 
@@ -43,6 +44,31 @@ function InventoryList() {
                             </div>
                     </Link>
                 </div>   
+            </div>
+            <div className="inventory-list__names names">
+                <div className="names__column names__column names__item">
+                    <h4 className="names__title">Inventory item</h4>
+                    <img className="names__sort-icon" src={SortIcon} alt="Sort Icon" />
+                </div>
+                <div className="names__column names__column names__category">
+                    <h4 className="names__title">Category</h4>
+                    <img className="names__sort-icon" src={SortIcon} alt="Sort Icon" />
+                </div>
+                <div className="names__column names__column names__status">
+                    <h4 className="names__title">Status</h4>
+                    <img className="names__sort-icon" src={SortIcon} alt="Sort Icon" />
+                </div>
+                <div className="names__column names__column names__qty">
+                    <h4 className="names__title">Qty</h4>
+                    <img className="names__sort-icon" src={SortIcon} alt="Sort Icon" />
+                </div>
+                <div className="names__column names__column names__warehouse">
+                    <h4 className="names__title">Warehouse</h4>
+                    <img className="names__sort-icon" src={SortIcon} alt="Sort Icon" />
+                </div>
+                <div className="names__column names__column names__actions">
+                    <h4 className="names__title">Actions</h4>
+                </div>
             </div>
             <div className="inventory-list__bottom">
                 {inventory.map((inventory, i)=>{
