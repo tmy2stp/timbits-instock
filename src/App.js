0ccec884fb38e44d30086c90d6c01944f7c89565
Ani Mihaylova <anivm.webdev@gmail.com>
2022-09-11 17:10:21 -0400

    test and play around to get the header and titles to align

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             <div className="background__card">
               <Routes>
 
-              <Route path="/"/>
+              <Route path="/" element={<WarehouseListPage />}/>
               <Route path="/inventory" element={<InventoryList />}/>
               <Route path="/warehouses" element={<WarehouseListPage />}/>
               <Route path="/warehouses/warehouse/:id" element={<WarehouseDetailsPage />}/>
